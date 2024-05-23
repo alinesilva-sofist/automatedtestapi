ff4bef9e07f4aac7064acb1d46b604c44c76ee42
Aline Silva <aline.silva@sofist.co>
2024-05-23 10:13:22 -0300

    Inclusao do faker no teste buscaProdutosCadastradosFakerGPT

--- AutomatedTestsApiProject/src/test/java/apiQA/AppTest.java
+++ AutomatedTestsApiProject/src/test/java/apiQA/AppTest.java
@@ -4,10 +4,15 @@ import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 
+import com.github.javafaker.Faker;
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.hasItem;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,6 +36,47 @@ public class AppTest {
                 body("Message", Matchers.equalTo("there are stocks in the database")).
                 body("Content.Stocks[0].Name", Matchers.equalTo("CSN MINERACAO"));
 
+    }
+
+    @Test
+    public void buscaProdutosCadastradosFakerGPT() {
+//Gerar valores dinâmicos para cadastrar ação
+        Faker faker = new Faker();
+        String fakeName = faker.company().name();
+        float fakePrice = faker.number().randomNumber(2, true);
+        String fakeSymbol = faker.regexify("[A-Z]{4}[0-9]{1,2}");
+//Cadastrar ação
+        String json = "{\n" +
+        "    \"Name\": \""+fakeName+"\",\n" +
+        "    \"Symbol\": \""+fakeSymbol+"\",\n" +
+        "    \"Price\": "+fakePrice+"\n" +
+        "}";
+  
+        RestAssured.baseURI = "http://127.0.0.1:5000";
+        given().
+            contentType(ContentType.JSON).
+            body(json).
+            log().all().
+        when().
+            post("new/stock/"). 
+        then().
+            assertThat().
+                statusCode(201);
+       
+//Procurar ações
+        RestAssured.baseURI = "http://127.0.0.1:5000/";
+        Response response = given().
+            contentType(ContentType.JSON).
+            log().all(). /*Validar o Faker na request*/
+        when().  
+            get("stocks").
+        then().
+            assertThat().
+                statusCode(200).
+                log().all().
+                body("Message", equalTo("there are stocks in the database"))
+                .body("Content.Stocks.Name", hasItem(fakeName))
+                .extract().response();
     }
 
     @Test
